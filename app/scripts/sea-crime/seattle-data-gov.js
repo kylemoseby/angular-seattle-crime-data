ec63927cb916795f9300dd9bd8d9431224c955d9
Kyle Moseby <kyle@kylemoseby.com>
2016-10-19 16:41:58 -0700

    dist working

--- app/scripts/sea-crime/seattle-data-gov.js
+++ app/scripts/sea-crime/seattle-data-gov.js
@@ -13,94 +13,28 @@ angular.module('mkm.seaCrimeData', [
         // url: 'scripts/sea-crime/7ais-f98f.json'
     });
 
+    var $fillColor = d3.scale.category20();
+
+    function getIncidentParent(report) {
+      return (report.indexOf('-') === -1) ?
+        report :
+        report.slice(0, report.indexOf('-'));
+    }
+
     function processIncidentData(data) {
-      /*
-          Loops through data returns from 'https://data.seattle.gov/resource/7ais-f98f.json'
-          Establishes parent/child relationships from offense description
-          Assigns incident colors based up incident catefories
-          creates a GEOJSON object to pass to mapCanvas directive compatable with Google Maps API standards
-      */
 
-      function indexUpdate(_report_) {
+      var _plots_ = [];
 
-        //  PARENT TYPE
-        var offType = _report_.offense_type;
-
-        var parentType = (offType.indexOf('-') === -1) ? offType : offType.slice(0, offType.indexOf('-'));
-
-        function updateIndex(typeStr, currentChildren) {
-
-          if (typeStr !== 'VEH-THEFT-AUTO') {
-
-            var newChildren = getChildType(typeStr);
-
-            for (var i = newChildren.length - 1; i >= 0; i--) {
-              var childType = newChildren[i];
-
-              if (currentChildren.hasOwnProperty(childType)) {
-                currentChildren[childType].count++;
-              } else {
-                currentChildren[childType] = {
-                  $type: childType,
-                  count: 1
-                };
-              }
-            }
-          }
-
-          return currentChildren;
-        }
-
+      for (var i = data.length - 1; i >= 0; i--) {
         /*
-            Returns all childtypes from index
+          GET report object
 
-            requires report full report type string
-
-            uses indexof('-') to splice string and get childtypes
+          extends _mapBounds and adds a GEOJSON report object to _plots_
         */
-        function getChildType(typeStr) {
+        var _report_ = data[i];
 
-          var q = typeStr;
-          var childTypes = [];
+        _report_.fillColor = $fillColor(getIncidentParent(_report_.offense_type));
 
-          while (q.lastIndexOf('-') !== -1) {
-            childTypes.push(q.slice(q.lastIndexOf('-') + 1, q.length));
-
-            q = q.slice(0, q.lastIndexOf('-'));
-          }
-
-          return childTypes;
-        }
-
-        if (_index_.hasOwnProperty(parentType)) {
-
-          var childrenNow = _index_[parentType].children;
-
-          _index_[parentType].children = updateIndex(offType, childrenNow);
-
-          _index_[parentType].count++;
-
-        } else if (parentType !== undefined) {
-
-          // if code is node found a new $inex object property is created
-          _index_[parentType] = {
-            'count': 1,
-            'offenseCategory': offType === 'VEH-THEFT-AUTO' ? offType : parentType,
-            'show': true,
-            'children': null
-          };
-
-          _index_[parentType].children = updateIndex(offType, {});
-
-        }
-      }
-
-      /*
-        GET report object
-
-        extends _mapBounds and adds a GEOJSON report object to _plots_
-      */
-      function plotsAddReport(_report_) {
         var _longitude = Number(_report_.longitude);
         var _latitude = Number(_report_.latitude);
 
@@ -112,71 +46,46 @@ angular.module('mkm.seaCrimeData', [
             'type': 'Feature',
             'geometry': {
               'type': 'Point',
-              'coordinates': [_longitude, _latitude]
+              'coordinates': [_longitude, _latitude],
             },
             'properties': _report_
           });
 
         }
-
       }
-
-      function setIndexColors() {
-
-        // function colorBrewer(ind) {
-        //   var brew = [
-        //     '#1f78b4',
-        //     '#b2df8a',
-        //     '#33a02c',
-        //     '#fb9a99',
-        //     '#a6cee3',
-        //     '#e31a1c',
-        //     '#fdbf6f',
-        //     '#ff7f00',
-        //     '#cab2d6',
-        //     '#6a3d9a',
-        //     '#ffff99',
-        //     '#b15928'
-        //   ];
-
-        //   return brew[ind % brew.length];
-        // }
-
-        // var q = 0;
-
-        // for (var key in _index_) {
-
-        //   _index_[key].fillColor = colorBrewer(q);
-
-        //   q++;
-        // }
-
-        var d3Colour = d3.scale.category20()
-          .domain(d3.keys(_index_));
-
-        for (var key in _index_) {
-          _index_[key].fillColor = d3Colour(key);
-        }
-      }
-
-
-      var _index_ = {};
-
-      var _plots_ = [];
-
-      for (var i = data.length - 1; i >= 0; i--) {
-
-        plotsAddReport(data[i]);
-
-        indexUpdate(data[i]);
-      }
-
-      setIndexColors();
 
       return {
-        reports: _plots_,
-        index: _index_
+        reports: _plots_
       };
+    }
+
+    function buildIndex(data) {
+
+      var $indxOffType = d3.nest()
+        .key(function(d) {
+          try {
+            return getIncidentParent(d.offense_type);
+            // return d.offense_type;
+          } catch (e) {
+            console.log(e);
+            console.log(d);
+          }
+        })
+        .entries(data);
+
+      $fillColor.domain($indxOffType
+        .map(function(d) {
+          return d.key;
+        })
+        .reverse());
+
+      return $indxOffType.map(function(d) {
+
+        d.fillColor = $fillColor(d.key);
+
+        return d;
+
+      });
     }
 
     /*
@@ -188,11 +97,17 @@ angular.module('mkm.seaCrimeData', [
 
     _http
       .then(function(response) {
-        var processedData = processIncidentData(response.data);
+
+        var $index = buildIndex(response.data);
+
+        var processedData = processIncidentData(response.data, $index);
 
         _promise.resolve({
           'incidents': processedData.reports,
-          'index': processedData.index,
+          'index': $index.sort(function(a, b) {
+              return d3.ascending(a.values.length, b.values.length);
+            })
+            .reverse(),
           'mapBounds': _mapBounds
         });
       });
