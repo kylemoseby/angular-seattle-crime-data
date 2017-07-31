605a3b3f0fd4d4038e2190401e28fd8d4883e53d
Kyle Moseby <kyle@kylemoseby.com>
2017-07-31 12:37:58 -0700

    Basic functionality

--- app/scripts/sea-crime/crime-reports-plotted.js
+++ app/scripts/sea-crime/crime-reports-plotted.js
@@ -3,20 +3,426 @@
 angular
   .module('mkm.crimeDemo', [
     'ngMaterial',
+    '$mdPanel',
     'mkm.seaCrimeData'
   ]);
 
 angular.module('mkm.seaCrimeData')
-  .controller('PlottedReportrsCtrl', ['$scope', '$http',
-    function($scope, $http) {
+  .directive("seattleCrimePlotted", ['$http', function($http) {
+
+    function _controller_($scope) {
 
       $scope._seaCrimeData_ = $http({
         method: 'GET',
         url: 'https://data.seattle.gov/resource/7ais-f98f.json'
       });
+
+      // https://data.seattle.gov/Public-Safety/SPD-Offense-Codes/22zb-azac/about
+      $scope._offenseCodes_ = $http({
+          method: 'GET',
+          url: 'https://data.seattle.gov/resource/aj7i-nahf.json'
+        })
+        .then(function(response) {
+          $scope.offCodeMap = d3.map(response.data, function(d) {
+            return d.summary_offense_code;
+          });
+        });
+
+      // $scope.loaded = true;
+
+      // DISPLAY OPTIONS
+      $scope.disp = {
+        axis: 'district', // district || extension
+        color: 'extension', // district || extension
+        circRad: 5 // 5 - 20  applied as vh
+      };
+      // END DISPLAY OPTIONS
+
+      $scope.filter = {
+        extension: {},
+        district: {}
+      };
+
+
+      var $scales = $scope.scales = {
+
+        dateReported: d3.scaleTime(),
+
+        extBand: d3.scaleBand(),
+
+        extColour: d3.scaleOrdinal(d3.schemeCategory20),
+
+        distBand: d3.scaleBand(),
+
+        distColour: d3.scaleOrdinal(d3.schemeCategory20b)
+      };
+
+      $scope.setElementScales = function(_svg_) {
+
+        var svgElm = _svg_.node();
+
+        $scales.dateReported
+          .range([40, svgElm.width.baseVal.value - 40]);
+
+        $scales.distBand
+          .range([40, svgElm.height.baseVal.value - 40]);
+
+        $scales.extBand
+          .range([40, svgElm.height.baseVal.value - 40]);
+      };
+
+      $scope.setDataScales = function(dataArr) {
+
+        // Date range for Crime Reports
+        var dateReportedExt = d3.extent(dataArr, function(d) {
+          return new Date(d.date_reported);
+        });
+
+
+        $scales.dateReported
+          .domain(dateReportedExt);
+
+
+        var distrists = [];
+        var offenseCode = [];
+
+        dataArr.forEach(function(d) {
+
+          if (!distrists.includes(d.district_sector)) {
+            distrists.push(d.district_sector);
+          }
+
+          if (!offenseCode.includes(d.summary_offense_code)) {
+            offenseCode.push(d.summary_offense_code);
+          }
+        });
+
+        $scales.extBand
+          .domain(offenseCode);
+
+        $scales.distBand
+          .domain(distrists);
+      };
+
+      $scope._seaCrimeData_
+        .finally(function() {
+          // $scope.loaded = true;
+        });
     }
-  ])
+
+    return {
+      controller: _controller_,
+    };
+  }])
+  .directive('crimePlotMenu', [function() {
+    return {
+      require: '^seattleCrimePlotted',
+      templateUrl: 'views/template-crime-plot-menu.html',
+      link: function($scope) {
+
+        $scope.isIndeterminate = function() {
+          return ($scope.selected.length !== 0 &&
+            $scope.selected.length !== $scope.items.length);
+        };
+
+        $scope.isChecked = function(type) {
+
+          var _filter_ = $scope.filter[type];
+
+          var checked = true;
+
+          if (Object.keys(_filter_).length > 0) {
+
+            for (var key in _filter_) {
+
+              if (!_filter_[key]) {
+
+                checked = false;
+                break;
+              }
+            }
+          }
+
+          return checked;
+        };
+
+        $scope.toggleAll = function(type) {
+
+          var vals = Object.values($scope.filter[type]);
+
+          var $test = vals.every(function(d) {
+            return d === true;
+          });
+
+          for (var key in $scope.filter[type]) {
+            $scope.filter[type][key] = !$test;
+          }
+
+          $scope.filterCircle();
+        };
+      }
+    };
+  }])
+  .directive('crimePlotSvg', ['$window', 'crimeReportPanelPlots', function($window, $reportPanel) {
+    return {
+      require: ['^seattleCrimePlotted'],
+      link: function($scope, $element) {
+
+        var $panel = $reportPanel();
+
+        // SVG element cast as D3
+        var $svg = d3
+          .select($element[0])
+          .append('svg');
+
+        var xAxis = d3.axisBottom()
+          .ticks(d3.timeHour, 24)
+          .tickFormat(d3.timeFormat('%m/%d'));
+
+        var yAxisDist = d3.axisLeft();
+        var yAxisExt = d3.axisLeft();
+
+        var axDate = $svg.append('g')
+          .attr('class', 'axis x');
+
+
+        // DISPLAY DISTRICTS ON INIT()
+        var axDist = $svg.append('g')
+          .attr('transform', 'translate(35, -22)')
+          .attr('class', 'axis y dist')
+          .attr('opacity', 1);
+
+        // APPEND X AXIS
+        var axExt = $svg.append('g')
+          .attr('transform', 'translate(35, -22)')
+          .attr('class', 'axis y type')
+          .attr('opacity', 0); // hide code extension axis
+
+
+        // CRIME REPORT $scope.CIRCLES
+        var circWrap = $svg.append('g')
+          .attr('class', 'crime-report-plots');
+
+        var $scales = $scope.scales;
+
+
+        $scope._seaCrimeData_.then(function init(response) {
+
+          var crimeReports = response.data;
+
+          $svg
+            .attr('height', $element[0].offsetHeight)
+            .attr('width', $element[0].offsetWidth);
+
+
+          // CALCULATED SCALES OBJECT
+          $scope.setElementScales($svg);
+          $scope.setDataScales(crimeReports);
+
+          // AXIS CLASSES
+          function callAxis(_scales_) { // X AXIS
+
+            xAxis.scale(_scales_.dateReported);
+
+            // APPEND X AXIS
+            axDate.call(xAxis);
+
+            // Y AXIS
+            yAxisDist.scale(_scales_.distBand);
+            yAxisExt.scale(_scales_.extBand);
+
+            axDist.call(yAxisDist);
+            axExt.call(yAxisExt);
+          }
+
+          callAxis($scales);
+
+
+          function calcCircR(d) {
+
+            if ($scope.filter.extension[d.summary_offense_code] &&
+              $scope.filter.district[d.district_sector]) {
+
+              return ($scope.disp.circRad / 10) + 'vh';
+
+            } else {
+
+              return '0px';
+            }
+          }
+
+
+          $scope.circles = circWrap.selectAll('circle')
+            .data(response.data)
+            .enter()
+            .append('circle')
+            .attr('r', calcCircR)
+            .attr('class', 'timeline-circle')
+            .attr('cx', function(d) {
+              return $scales.dateReported(new Date(d.date_reported));
+            })
+            .attr('cy', function(d) {
+              return $scales.distBand(d.district_sector); //plot by district
+            })
+            .style('fill', function(d) {
+              // populates district scale
+
+              $scales.distColour(d.district_sector);
+
+              return $scales.extColour(d.summary_offense_code); // fill by code extension
+            })
+            .on('click', function(d) {
+              $panel.showPanel(d, d3.event, this);
+            });
+
+          // https://davidwalsh.name/essential-javascript-functions
+          // Returns a function, that, as long as it continues to be invoked, will not
+          // be triggered. The function will be called after it stops being called for
+          // N milliseconds. If `immediate` is passed, trigger the function on the
+          // leading edge, instead of the trailing.
+          function debounce(func, wait, immediate) {
+            var timeout;
+            return function() {
+              var context = this,
+                args = arguments;
+              var later = function() {
+                timeout = null;
+                if (!immediate) { func.apply(context, args); }
+              };
+              var callNow = immediate && !timeout;
+              clearTimeout(timeout);
+              timeout = setTimeout(later, wait);
+              if (callNow) { func.apply(context, args); }
+            };
+          }
+
+
+          // Transistions definitions
+          var t = d3.transition()
+            .duration(950)
+            .ease(d3.easeLinear);
+
+          $scope.setCircleRad = debounce(function() {
+            $scope.circles
+              .transition(t)
+              .attr('r', calcCircR);
+
+          }, 250);
+
+          $scope.filterCircle = debounce(function() {
+
+            $scope.circles.transition(t)
+              .attr('r', calcCircR);
+
+          }, 250);
+
+
+          // Event Handlers
+          $scope.circSetColorType = function() {
+
+            $scope.circles.transition(t)
+              .style('fill', function(d) {
+                return $scales.extColour(d.summary_offense_code);
+              });
+
+            $scope.disp.color = 'extension';
+          };
+
+          $scope.circSetColorDist = function() {
+
+            $scope.circles.transition(t)
+              .style('fill', function(d) {
+                return $scales.distColour(d.district_sector);
+              });
+
+            $scope.disp.color = 'district';
+          };
+
+
+
+
+          function plotDistricts() {
+
+            axDist.attr('opacity', 1);
+            axExt.attr('opacity', 0);
+
+            $scope.circles.transition(t)
+              .attr('cy', function(d) {
+                return $scales.distBand(d.district_sector); //plot by district
+              });
+          }
+
+
+          function plotCodeExt() {
+
+            axDist.attr('opacity', 0);
+            axExt.attr('opacity', 1);
+
+            $scope.circles.transition(t)
+              .attr('cy', function(d) {
+                return $scales.extBand(d.summary_offense_code); //plot by offense code
+              });
+          }
+
+
+          $scope.toggleAxis = function() {
+
+            if ($scope.disp.axis === 'extension') {
+
+              plotDistricts();
+
+              $scope.disp.axis = 'district';
+
+            } else if ($scope.disp.axis === 'district') {
+
+              plotCodeExt();
+
+              $scope.disp.axis = 'extension';
+            }
+          };
+
+
+          angular.element($window).bind('resize', debounce(function() {
+
+            $svg
+              .attr('height', $element[0].offsetHeight)
+              .attr('width', $element[0].offsetWidth);
+
+            $scope.setElementScales($svg);
+
+            $scope.circles.transition(t)
+              .attr('cx', function(d) {
+                return $scales.dateReported(new Date(d.date_reported));
+              })
+              .attr('cy', function(d) {
+
+                if ($scope.disp.axis === 'district') {
+
+                  return $scales.distBand(d.district_sector);
+
+                } else if ($scope.disp.axis === 'extension') {
+
+                  return $scales.extBand(d.summary_offense_code);
+                }
+              });
+
+            callAxis($scales);
+          }, 250));
+
+
+
+
+
+          // UPDATE DOM
+          $scope.scaleType = $scales.extColour;
+
+          $scope.scaleDist = $scales.distColour;
+        });
+      }
+    };
+  }])
   .factory('crimeReportPanelPlots', ['$mdPanel', function($mdPanel) {
+
     var $panel = $mdPanel;
 
     function _showPanel(d, $event, d3elm) {
@@ -34,9 +440,8 @@ angular.module('mkm.seaCrimeData')
         .withAnimation($mdPanel.animation.SCALE);
 
       var config = {
-        attachTo: $elm,
-        // attachTo: angular.element(document.body),
-        // controller: DialogController,
+        attachTo: angular.element(document.body),
+        controller: DialogController,
         controllerAs: 'ctrl',
         position: panelPosition,
         animation: panelAnimation,
@@ -51,297 +456,29 @@ angular.module('mkm.seaCrimeData')
         }
       };
 
-      $mdPanel.open(config)
+      $panel.open(config)
         .then(function(result) {
           $panel = result;
         });
     }
 
-    // function DialogController(MdPanelRef) {
-    //   function closeDialog() {
-    //     if (MdPanelRef) {
-    //       MdPanelRef.close();
-    //     }
-    //   }
-    // }
+    function DialogController($scope, mdPanelRef, report) {
+
+      $scope.report = report;
+      $scope.keys = Object.keys(report);
+
+      $scope.closeDialog = function () {
+        if (mdPanelRef) {
+          mdPanelRef.close();
+        }
+      };
+    }
 
 
     return function reportPanel() {
       return {
-        test: 'kylemoseby',
         showPanel: _showPanel
       };
     };
-  }])
-  .directive("seattleCrimePlotted", ['$window', 'crimeReportPanelPlots', function($window, $reportPanel) {
-
-    function _link_($scope, $element) {
-
-      function calcScales(dataArr, _svg_) {
-        //         Get X scale info
-
-        var svgElm = _svg_.node();
-
-        // Date range for Crime Reports
-        var dateReportedExt = d3.extent(dataArr, function(d) {
-          return new Date(d.date_reported);
-        });
-
-        //         Get Y scale info
-        // var disrictMap = dataArr.map(function(d) {
-        //   return d.district_sector;
-        // });
-        // console.log(disrictMap);
-        // var codeExtMap = dataArr.map(function(d) {
-        //   return d.summary_offense_code;
-        //   // return d.summary_offense_code;
-        // });
-        // console.log(codeExtMap);
-
-        var distrists = [];
-        var offenseCode = [];
-
-        dataArr.forEach(function(d) {
-
-          if (!distrists.includes(d.district_sector)) {
-            distrists.push(d.district_sector);
-          }
-
-          if (!offenseCode.includes(d.summary_offense_code)) {
-            offenseCode.push(d.summary_offense_code);
-          }
-        });
-
-        // RETURNED SCALES
-        return {
-
-          dateReported: new d3.scaleTime()
-            .domain(dateReportedExt)
-            .range([20, svgElm.width.baseVal.value - 20]),
-
-
-          extBand: new d3.scaleBand()
-            .domain(offenseCode)
-            .range([20, svgElm.height.baseVal.value - 20]),
-
-
-          extColour: new d3.scaleOrdinal(d3.schemeCategory20b),
-
-
-          distBand: new d3.scaleBand()
-            .domain(distrists)
-            .range([20, svgElm.height.baseVal.value - 20]),
-
-
-          distColour: new d3.scaleOrdinal(d3.schemeCategory20)
-          // END RETURNED SCALES
-        };
-      }
-
-
-
-      $scope.loaded = false;
-
-      var $panel = $reportPanel();
-
-
-      // DISPLAY OPTIONS
-      var viewopts = {
-        axis: 'district', // district || extension
-        color: 'extension' // district || extension
-      };
-      // END DISPLAY OPTIONS
-
-      function init(response) {
-
-
-        var crimeReports = response.data;
-
-        // FIX ELEMENT LOOKUPS
-        // SVG element cast as D3
-        var $svg = d3.select(
-            $element.children()[0]
-          )
-          .attr('height', $element[0].offsetHeight)
-          .attr('width', $element[0].offsetWidth);
-
-        // CALCULATED SCALES OBJECT
-        var $scales = calcScales(crimeReports, $svg);
-
-        // AXIS CLASSES
-        // X AXIS
-        var xAxis = d3.axisBottom()
-          .ticks(d3.timeHour, 24)
-          .tickFormat(d3.timeFormat('%m/%d'))
-          .scale($scales.dateReported);
-
-        // APPEND X AXIS
-        $svg.append('g')
-          .attr('class', 'axis x')
-          .call(xAxis);
-
-
-
-        // Y AXIS
-        var yAxisDist = d3.axisLeft()
-          .scale($scales.distBand);
-
-        var yAxisExt = d3.axisLeft()
-          .scale($scales.extBand);
-
-        // DISPLAY DISTRICTS ON INIT()
-        var axDist = $svg.append('g')
-          .attr('transform', 'translate(20, -22)')
-          .attr('class', 'axis y dist')
-          .attr('opacity', 1)
-          .call(yAxisDist);
-
-        // APPEND X AXIS
-        var axExt = $svg.append('g')
-          .attr('transform', 'translate(20, -22)')
-          .attr('class', 'axis y type')
-          .attr('opacity', 0) // hide code extension axis
-          .call(yAxisExt);
-
-
-
-        // CRIME REPORT CIRCLES
-        var circWrap = $svg.append('g')
-          .attr('class', 'crime-report-plots');
-
-        var circles = circWrap.selectAll('circle')
-          .data(response.data)
-          .enter()
-          .append('circle')
-          .attr('r', '.5vh')
-          .attr('class', 'timeline-circle')
-          .attr('cx', function(d) {
-            return $scales.dateReported(new Date(d.date_reported));
-          })
-          .attr('cy', function(d) {
-            return $scales.distBand(d.district_sector); //plot by district
-          })
-          .style('fill', function(d) {
-            return $scales.extColour(d.summary_offense_code); // fill by code extension
-          })
-          .on('click', function(d) {
-            $panel.showPanel(d, d3.event, this);
-          });
-
-
-        // Transistions definitions
-        var t = d3.transition()
-          .duration(950)
-          .ease(d3.easeLinear);
-
-
-        function circSetStyleCodeExt() {
-
-          circles.transition(t)
-            .style('fill', function(d) {
-              return $scales.extColour(d.summary_offense_code);
-            });
-
-          viewopts.color = 'extension';
-        }
-
-
-
-        function circSetStyleDist() {
-
-          circles.transition(t)
-            .style('fill', function(d) {
-              return $scales.distColour(d.district_sector);
-            });
-
-          viewopts.color = 'district';
-        }
-
-
-
-        // Event Handlers
-        $scope.circSetColorType = circSetStyleCodeExt;
-        $scope.circSetColorDist = circSetStyleDist;
-
-        function plotDistricts() {
-          axDist.attr('opacity', 1);
-
-          axExt.attr('opacity', 0);
-
-          circles.transition(t)
-            .attr('cy', function(d) {
-              return $scales.distBand(d.district_sector); //plot by district
-            });
-
-        }
-
-
-        function plotCodeExt() {
-          axDist.attr('opacity', 0);
-
-          axExt.attr('opacity', 1);
-
-          circles.transition(t)
-            .attr('cy', function(d) {
-              return $scales.extBand(d.summary_offense_code); //plot by offense code
-            });
-
-        }
-
-
-        $scope.toggleAxis = function() {
-
-          if (viewopts.axis === 'extension') {
-
-            plotDistricts();
-
-            viewopts.axis = 'district';
-
-          } else if (viewopts.axis === 'district') {
-
-            plotCodeExt();
-
-            viewopts.axis = 'extension';
-          }
-        };
-
-
-        // UPDATE DOM
-        $scope.scaleType = $scales.extColour;
-
-        $scope.scaleDist = $scales.distColour;
-
-      }
-
-
-      angular.element($window).bind('resize', function() {
-        console.log('resize plots for new window');
-      });
-
-
-
-
-      $scope._seaCrimeData_
-        .then(init)
-        .finally(function() {
-          $scope.loaded = true;
-        });
-    }
-
-    return {
-      // name: '',
-      // priority: 1,
-      // terminal: true,
-      // scope: {
-      //   reportsData: '=reports'
-      // }, // {} = isolate, true = child, false/undefined = no change
-      // controller: function($scope, $element, $attrs, $transclude) {},
-      // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-      // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-      // templateUrl: '',
-      // replace: true,
-      // transclude: true,
-      // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-      link: _link_
-    };
   }]);
+
